dec88af93c3c39e34415ee4ce3e338d9f48a159b
FokinRoman <116208163+FokinRoman@users.noreply.github.com>
2022-10-24 15:54:47 +0300

    Update task_9.py

--- Лабораторная работа 3(1)/task_9.py
+++ Лабораторная работа 3(1)/task_9.py
@@ -2,16 +2,13 @@ salary = 5000  # зарплата
 spend = 6000  # траты
 months = 10  # количество месяцев
 increase = 0.03  # рост цен
-money = 0
+need_money = 0
+month_s = months  # сохраняю значение переменной в другой
 
-while months != 0:
-
-    money += spend
+while months != 0:  # пустая строка выше отделяет блок с циклом
+    need_money += spend
     spend = spend * (1 + increase)
     months -= 1
 
-money -= salary * 10
-
-need_money = round(money)  # количество денег, чтобы прожить 10 месяцев
-
+need_money -= salary * month_s  # количество денег, чтобы прожить 10 месяцев
 print(round(need_money))
